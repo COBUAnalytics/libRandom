e41880941fd1354a78b2ddc742719b5b94ac9d95
carminemangione <carmine@mangione.com>
2013-11-11 10:12:07 -0800

    removed unused import

---
+++ src/main/java/org/cobu/randomsamplers/KMeansSampler.java
@@ -0,0 +1,8 @@
+package org.cobu.randomsamplers;
+
+public class KMeansSampler {
+    // Run across all data with sampler on reservoir size of 1 for each of N clusters
+    // at first centroid count is zero (use 1 as weight to get random point)
+    // after that, number of centroids you have already selected
+    // Once all centroids have been selected, rerun to get sample of size K
+}
