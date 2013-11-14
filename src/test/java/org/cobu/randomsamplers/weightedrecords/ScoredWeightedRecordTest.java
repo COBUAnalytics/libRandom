5ce21c5ea32ae1057def829ae8b46dbe2152e636
carminemangione <carmine@mangione.com>
2013-11-14 14:56:56 -0800

    Fixed formatting

--- src/test/java/org/cobu/randomsamplers/weightedrecords/ScoredWeightedRecordTest.java
+++ src/test/java/org/cobu/randomsamplers/weightedrecords/ScoredWeightedRecordTest.java
@@ -19,19 +19,19 @@ public class ScoredWeightedRecordTest {
     public void twoWeightedRecordsSameWeight() {
         ScoredWeightedRecord one = createScoredWeightedRecord(1.0, 1.0);
         ScoredWeightedRecord two = createScoredWeightedRecord(0.5, 1.0);
-        assertEquals(-1,one.compareTo(two));
+        assertEquals(-1, one.compareTo(two));
     }
 
     @Test
     public void twoScoresSmallIncrementOnExponent() {
         double random = 0.5;
-        ScoredWeightedRecord one = createScoredWeightedRecord(random,1.0);
+        ScoredWeightedRecord one = createScoredWeightedRecord(random, 1.0);
 
-        ScoredWeightedRecord slightlyLarger = createScoredWeightedRecord(random * random+1.0e-14,2.0);
-        ScoredWeightedRecord slightlySmaller = createScoredWeightedRecord(random * random-1.0e-14,2.0);
+        ScoredWeightedRecord slightlyLarger = createScoredWeightedRecord(random * random + 1.0e-14, 2.0);
+        ScoredWeightedRecord slightlySmaller = createScoredWeightedRecord(random * random - 1.0e-14, 2.0);
 
-        assertEquals(1,one.compareTo(slightlyLarger));
-        assertEquals(-1,one.compareTo(slightlySmaller));
+        assertEquals(1, one.compareTo(slightlyLarger));
+        assertEquals(-1, one.compareTo(slightlySmaller));
     }
 
 }
